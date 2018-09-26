ba6c9b7739cf95f5b69aa2765fd46cb14207d363
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-8.local>
2018-09-26 12:58:56 +0900

    fix

--- image2vector.py
+++ image2vector.py
@@ -6,8 +6,8 @@ import sys
 import glob
 import pickle
 
-trimmed_image_path = "./database/img_align_celeba"
-trimmed_images = glob.glob(trimmed_image_path + "/*")
+trimmed_image_path = "./database"
+trimmed_images = glob.glob(trimmed_image_path + "/*.jpg")
 print(trimmed_images)
 
 
@@ -15,6 +15,7 @@ vector_images = {}
 
 for image_file in trimmed_images:
     image = face_recognition.load_image_file(image_file)
+
     face_encoding = face_recognition.face_encodings(image)[0]
     vector_images[image_file.split("/")[-1]] = face_encoding.tolist()
 
